22bea421afde5af2f975abed078553f3432bfc86
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2016-11-28 17:57:40 +0100

    added WebSocket demo implementation using STOMP

---
+++ src/main/java/net/whisperersuite/server/model/Greeting.java
@@ -0,0 +1,16 @@
+package net.whisperersuite.server.model;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {}
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}

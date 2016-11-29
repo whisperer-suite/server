a8681f306a97568afc250c3ff7f8dc543f6db021
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2016-11-30 00:42:50 +0100

    switched demo from STOP to pure WebSockets

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,57 +1,22 @@
-var stompClient = null;
+$(document).ready(function() {
+    var sock = new SockJS('/messages');
+    sock.onopen = function() {
+        console.log('opened');
+        $('#loading').toggleClass("hidden-xs-up");
+        $('#app').toggleClass("hidden-xs-up");
+        this.onmessage = function(e) {
+            $('#messages').append('<br/>server: ' + e.data);
+        };
+        $('#msgForm').submit(function() {
+            var msg = $('#msg');
+            $('#messages').append('<br/>me: ' + msg.val());
+            sock.send(msg.val());
+            msg.val("");
 
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
-}
-
-function connect() {
-    var socket = new SockJS('/gs-guide-websocket');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/greetings', function (greeting) {
-            showGreeting(JSON.parse(greeting.body).content);
+            return false;
         });
-    });
-}
-
-function disconnect() {
-    if (stompClient != null) {
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
-}
-
-function sendName() {
-    stompClient.send(
-        "/app/hello",
-        {},
-        JSON.stringify({
-            'name': $("#name").val(),
-            'createdAt': Date.now()
-        })
-    );
-}
-
-function showGreeting(message) {
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
-}
-
-$(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+    };
+    sock.onclose = function() {
+        console.log('closed');
+    };
 });

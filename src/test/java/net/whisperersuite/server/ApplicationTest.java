19ff56a30deec80a62c7f064c9c466022d5005da
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2016-12-07 15:58:30 +0100

    fixed application test

--- src/test/java/net/whisperersuite/server/ApplicationTest.java
+++ src/test/java/net/whisperersuite/server/ApplicationTest.java
@@ -1,15 +1,29 @@
 package net.whisperersuite.server;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public class ApplicationTest {
+public class ApplicationTest  implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
     @Test
     public void contextLoads() throws Exception {
         // tests whether the app loads at all
+        assertNotNull(applicationContext);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }

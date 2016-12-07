7cdd8ac3416f7b60b6398beac03fe03e9d3f188a
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2016-12-07 14:28:20 +0100

    clean up some tests

--- src/test/java/net/whisperersuite/server/event/connection/ConnectionClosedEventTest.java
+++ src/test/java/net/whisperersuite/server/event/connection/ConnectionClosedEventTest.java
@@ -3,11 +3,12 @@ package net.whisperersuite.server.event.connection;
 import org.junit.Test;
 import org.springframework.web.socket.WebSocketSession;
 
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 
 public class ConnectionClosedEventTest {
     @Test
     public void constructor() throws Exception {
-        new ConnectionClosedEvent(mock(WebSocketSession.class));
+        assertNotNull(new ConnectionClosedEvent(mock(WebSocketSession.class)));
     }
 }

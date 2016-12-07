9760db1b45370736d18f8346dfbbc63a0dfe1175
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2016-12-07 12:29:10 +0100

    js demo app cleanup

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,31 +1,29 @@
 $(document).ready(function() {
-    var sock = new SockJS('/message');
+    let sock = new SockJS("/message");
     sock.onopen = function() {
-        console.log('opened');
-        $('#loading').toggleClass("hidden-xs-up");
-        $('#app').toggleClass("hidden-xs-up");
+        $("#loading").toggleClass("hidden-xs-up");
+        $("#app").toggleClass("hidden-xs-up");
+
         this.onmessage = function(e) {
-            console.log(e.data);
-            var event = JSON.parse(e.data);
-            if (event.type === 'MESSAGE') {
-                addMessage('<strong>' + event.payload.author + ':</strong> ' + event.payload.content);
-            } else if (event.type === 'USER_JOINED') {
-                addMessage('<strong> User joined: ' + event.payload.username + '</strong>');
-            } else if (event.type === 'USER_LEFT') {
-                addMessage('<strong> User left: ' + event.payload.username + '</strong>');
+            let event = JSON.parse(e.data);
+            if (event.type === "MESSAGE") {
+                addMessage("<strong>" + event.payload.author + ":</strong> " + event.payload.content);
+            } else if (event.type === "USER_JOINED") {
+                addMessage("<strong> User joined: " + event.payload.username + "</strong>");
+            } else if (event.type === "USER_LEFT") {
+                addMessage("<strong> User left: " + event.payload.username + "</strong>");
             }
         };
-        $('#msgForm').submit(function() {
-            var msg = $('#msg');
-            var content = msg.val();
-            if (content !== '') {
-                msg.val('');
-                addMessage('<strong>me:</strong> ' + content);
+
+        $("#msgForm").submit(function() {
+            let msg = $("#msg");
+            let content = msg.val();
+            if (content !== "") {
+                msg.val("");
+                addMessage("<strong>me:</strong> " + content);
                 sock.send(JSON.stringify({
-                    type: 'MESSAGE',
-                    payload: {
-                        content: content
-                    }
+                    type: "MESSAGE",
+                    payload: { content }
                 }));
             }
 
@@ -33,13 +31,12 @@ $(document).ready(function() {
         });
     };
     sock.onclose = function() {
-        console.log('closed');
-        addMessage('Disconnected')
+        addMessage("Disconnected");
     };
 });
 
 function addMessage(msg) {
-    var container = $('#messages');
-    container.append('<br/>' + msg);
+    let container = $("#messages");
+    container.append("<br/>" + msg);
     container.scrollTop(container[0].scrollHeight);
 }
